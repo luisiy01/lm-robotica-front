a21137c6cc3858dcfb9334d944dbc4595683b794
Luis Nunez <luisnunez91@gmail.com>
2026-04-14 13:18:26 -0600

    fix alumnos cuando el back responde mal

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
+import api from "../services/api";
 
-import api from '../services/api';
-
-export const loginUser = async (credentials: { email: string; password: string }) => {
+export const loginUser = async (credentials: {
+  email: string;
+  password: string;
+}) => {
+  try {
     const { data } = await api.post(`/auth/login`, credentials);
     return data; // Retorna { access_token, user: { id, nombre, rol } }
+  } catch (error) {
+    // Es CRUCIAL relanzar el error para que React Query lo detecte
+    throw error;
+  }
 };
